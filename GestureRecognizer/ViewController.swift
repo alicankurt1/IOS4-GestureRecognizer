3f73addaabe16bf773df6479c5b35135994d38f2
Alican Kurt <kurta15@itu.edu.tr>
2021-08-09 11:39:05 +0300

    Project Commit

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var carImageView: UIImageView!
+    
+    @IBOutlet weak var modelLabel: UILabel!
+    var counter = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        carImageView.isUserInteractionEnabled = true
+        
+        let carGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePicture))
+    
+        carImageView.addGestureRecognizer(carGestureRecognizer)
+        
+    }
+    
+    @objc func changePicture(){
+        counter = counter % 3
+        if counter == 0{
+            carImageView.image = UIImage(named: "gtr34")
+            modelLabel.text = "GT-R34"
+        }else if counter == 1{
+            carImageView.image = UIImage(named: "gtr35")
+            modelLabel.text = "GT-R35"
+        }else if counter == 2{
+            carImageView.image = UIImage(named: "gtr36")
+            modelLabel.text = "GT-R36"
+        }
+        counter += 1
+        
+        
+        
     }
 
 
